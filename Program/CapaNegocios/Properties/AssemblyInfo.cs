d2153f221364f6d5785eeae552c69999901e5886
Javier Luján <68826075+JavierL00@users.noreply.github.com>
2020-12-03 10:12:38 -0500

    Solución Capa Datos

--- Program/CapaNegocios/Properties/AssemblyInfo.cs
+++ Program/CapaNegocios/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
 // COM, establezca el atributo ComVisible en true en este tipo.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
 [assembly: Guid("f54502fd-a3ce-4dca-be7b-5f3b8a0f7802")]
